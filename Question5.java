02c95d9459ddc4cb72aebc6219ff25547a0273ce
Derekqua <50654603+Derekqua@users.noreply.github.com>
2020-04-25 20:57:20 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -25,8 +25,31 @@ public class Question5
      *     2
      * Hint: Use a loop to get input. Use another 2 loops to find the mode
      */
-     
+    int max = 0; //count highest mode
+    int modevalue = 0; //value of mode
     Scanner in = new Scanner(System.in);
-    
+    System.out.print("Enter number of inputs: ");
+    int noOfInputs = in.nextInt();
+
+    int[] intArray = new int[noOfInputs]; //create array
+
+    for (int i = 0; i < noOfInputs; i++) { //prompt to enter numbers
+      System.out.printf("Enter a number (%d): ", i+1);
+      intArray[i] = in.nextInt(); //insert numbers to array
+
+      int count = 0;
+      for (int j = 0; j<intArray.length; j++){
+
+        if(intArray[j] == intArray[i]){
+          count += 1; //if numbers are the same
+        }
+
+        if (count > max){
+          max = count;
+          modevalue = intArray[i];
+        }
+      }
+    }
+    System.out.print(modevalue); 
   }
 }
