a53b57eac2bee69101aee76da4400f9e2a52581c
Derekqua <50654603+Derekqua@users.noreply.github.com>
2020-04-25 19:40:45 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -11,6 +11,14 @@ public class Question2
      */
      
     Scanner in = new Scanner(System.in);
+    
+    System.out.print("Enter height in m: "); //Prompt for height
     double height = in.nextDouble();
+    
+    System.out.print("Enter Weight in Kg: "); //Prompt for weight   
+    double weight = in.nextDouble(); 
+    
+    double bmi = weight / (height * height); //Calculate bmi
+    System.out.println("Your bmi is " + bmi);
   }
 }
